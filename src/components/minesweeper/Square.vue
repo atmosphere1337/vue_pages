c1821fe4d2a8ceaa644609a309bfe47870ac77f3
atmosphere1337 <agbdlcider@gmail.com>
2023-07-22 02:05:04 +0800

    minesweeper. field almost done. extending and gameover cell block left.

--- src/components/minesweeper/Square.vue
+++ src/components/minesweeper/Square.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
     import {ref} from 'vue'
-    const val = ref('This is component')
+    const props = defineProps({
+        mode: {
+            type: Number,
+            default: 0
+        },
+        click_handler: {
+            type: Function,
+            default: ()=>{},
+        },
+        neighbour_handler: {
+            type: Function,
+            default: ()=>{}
+        },
+        pos: {
+            type: Number,
+            default: 0
+        },
+        vis: {
+            type: Number,
+            default: 0
+        } 
+    })
+    const neighbours = ref(0)
+    const avail = ref(true)
+    const killer = ref(false)
+    function handle()
+    {
+        if (avail.value)
+        {
+            avail.value = false
+            let keep:boolean = props.click_handler(props.pos)
+            if (keep)
+                neighbours.value = props.neighbour_handler(props.pos)
+            else
+                killer.value = true
+        }
+    }
 </script>
 <template>
-    <div class="m_cell">
-        x 
+    <div 
+     :class="{m_cell: true, m_clicked_empty: props.vis == 1, m_deadcell: killer}"
+     @click="handle()">
+       <!--{{ props.mode }} -->
+        <span v-if="props.mode == 0 && neighbours > 0">
+            {{neighbours}} 
+        </span>
+        <span v-if="props.vis == 1 && props.mode == 1">
+            X
+        </span>
     </div>
 </template>
--- src/views/Minesweeper.vue
+++ src/views/Minesweeper.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
-    import {ref} from 'vue';
+    import {ref, computed, toValue} from 'vue';
     import Square from '../components/minesweeper/Square.vue' 
-    
+    const width = ref(10)
+    const height = ref(10)
+    const bombs = ref(10)
+    const counter = ref(0)
+    function createArray(width:number, height:number): number[][]
+    {
+        let out: number[][] = new Array(height)
+        for (let i = 0; i < height; i++)
+            out[i] = new Array(width).fill(0)
+        return out;
+    }
     function createMap(width:number, height:number, times:number):number [][]
     {
         let size = width * height
@@ -16,16 +26,54 @@
             places.push(contender)
             linear_out[contender] = 1
         }
-        let out: number[][] = new Array(height)
-        for (let i = 0; i < )
+        let out: number[][] = createArray(width, height)
         for (let i = 0; i < height; i++) 
             for (let j = 0; j < width; j++)
-
+                out[i][j] = linear_out[width * i + j]
         return out
     }
-    const val = ref("Hello world")
-    const map = ref
-
+    function gridString(inp:number):string
+    {
+        let out:string = (100/inp).toString() + "%"
+        return out.repeat(inp).trim() 
+    }
+    function check_neighbours(pos:number):number
+    {
+        let y:number = Math.floor(pos/width.value)
+        let x:number = pos % height.value
+        let out:number = 0
+        out += (y + 1 >= height.value) ? 0 : map.value[y + 1][x]
+        out += (y + 1 >= height.value || x + 1 >= width.value) ? 0 : map.value[y + 1][x + 1]
+        out += (x + 1 >= width.value) ? 0 : map.value[y][x + 1]
+        out += (y - 1 < 0 || x + 1 >= width.value) ? 0 : map.value[y - 1][x + 1]
+        out += (y - 1 < 0) ? 0 : map.value[y - 1][x]
+        out += (y - 1 < 0 || x - 1 < 0) ? 0 : map.value[y - 1][x - 1]
+        out += (x - 1 < 0) ? 0 : map.value[y][x - 1]
+        out += (y + 1 >= height.value || x - 1 < 0) ? 0 : map.value[y + 1][x - 1]
+        return out
+    }
+    function press_click(counter:number):boolean
+    {
+        let y:number = Math.floor(counter/width.value)
+        let x:number = counter % height.value
+        map_visible.value[y][x] = 1
+        if (line_map.value[counter] == 1)
+        {
+            for (let i:number = 0; i < height.value; i++)
+                for (let j:number = 0; j < width.value; j++)
+                {
+                    if (map.value[i][j] == 1)
+                        map_visible.value[i][j] = 1
+                }
+            alert('you lost')
+            return false
+        }
+        return true
+    }
+    const map = ref(createMap(height.value,width.value,bombs.value))
+    const map_visible = ref(createArray(height.value, width.value))
+    const line_map = computed(()=>{return map.value.reduce((a:number[], b:number[])=>a.concat(b), [])})
+    const line_map_visible = computed(()=>{return map_visible.value.reduce((a:number[], b:number[])=>a.concat(b), [])})
     
 </script>
 <template>
@@ -33,8 +81,15 @@
         <div class="m_window">
             <div class="m_info"></div>
             <div class="m_outerfield">
-                <div class="m_field">
-                    <Square v-for="x in 16"></Square>
+                <div class="m_field" :style="{gridTemplateColumns: gridString(10), gridTemplateRows: gridString(10)}" >
+                    <Square
+                     v-for="(x, i) in line_map_visible"
+                     :vis="x"
+                     :pos="i"
+                     :mode="line_map[i]"
+                     :click_handler="press_click"
+                     :neighbour_handler="check_neighbours">
+                    </Square>
                 </div>
             </div>
         </div>
